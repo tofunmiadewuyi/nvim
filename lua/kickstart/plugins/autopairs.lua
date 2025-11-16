32ce5f5dc611eb6f7bb03186b3210a93182a9e63
7A <tiadewuyi@gmail.com>
2025-11-16 11:34:49 +0100

    initial setup

--- lua/kickstart/plugins/autopairs.lua
+++ lua/kickstart/plugins/autopairs.lua
@@ -5,4 +5,13 @@ return {
   'windwp/nvim-autopairs',
   event = 'InsertEnter',
   opts = {},
+  config = function()
+    require('nvim-autopairs').setup {
+      opts = {
+        enable_close = true, -- Auto close tags
+        enable_rename = true, -- Auto rename pairs of tags
+        enable_close_on_slash = false, -- Auto close on trailing </
+      },
+    }
+  end,
 }
